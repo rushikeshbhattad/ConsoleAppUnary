d4dfb3abee25445410cb225b6a5414d0eee29e19
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 14:34:32 +0530

    Add project files.

---
+++ ConsoleAppUnary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUnary
+{
+    class Unary_Operators
+    {
+        static void Main()
+        {
+            int x = 10;
+            int result;
+
+            Console.WriteLine("x is " + x);
+            result = x++;
+            Console.WriteLine("Post increment of x is " + x);
+
+            Console.WriteLine("x is " + x);
+            result = x--;
+            Console.WriteLine("Post decrement of x is " + x);
+
+            Console.WriteLine("x is " + x);
+            result = ++x;
+            Console.WriteLine("Pre increment of x is " + x);
+
+            Console.WriteLine("x is " + x);
+            result = --x;
+            Console.WriteLine("Pre decrement of x is " + x);
+            Console.ReadKey();
+        }
+    }
+}
